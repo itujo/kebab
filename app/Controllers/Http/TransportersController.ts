295fc23abb8c835ecfb2fec119bb957f9c59eca9
Ronaldo Junior <junhiowr@hotmail.com>
2021-11-11 12:03:24 -0300

    refactor: create direta route

--- app/Controllers/Http/TransportersController.ts
+++ app/Controllers/Http/TransportersController.ts
@@ -15,9 +15,9 @@ export default class TransportersController {
       name: 'diretalog',
       document: '08612193000143',
       address: 'AV MARQUES DE SAO VICENTE 682',
-      brdUser: '1',
-      brdPwd: '2',
-      apiToken: '123',
+      brdUser: '65b1a2df5ecb47fd0f945033661a6443',
+      brdPwd: '3e309cdbef2405367cce17446c8a1f812b049989f18bfccb265ff464016906b3',
+      apiToken: 'SPLOG',
       statusTable: 'simexpress_statuses',
     });
 
